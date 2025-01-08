d96bdd940e1dbe2183ed8d241526e768a84c8b82
nikitayakovina <36497484+nikitayakovina@users.noreply.github.com>
2025-01-08 21:59:05 +0300

    fix tests(vite)

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,15 @@ import handlebars from './src/vite-plugin-handlebars-precompile';
 
 export default defineConfig({
   plugins: [
-    handlebars()
+    handlebars(),
+    // {
+    //   name: 'polyfill-structured-clone',
+    //   config() {
+    //     if (typeof globalThis.structuredClone === 'undefined') {
+    //       globalThis.structuredClone = (obj) => JSON.parse(JSON.stringify(obj));
+    //     }
+    //   }
+    // }
   ],
-  publicDir: './src/static',
-  build: {
-    modulePreload: false
-  }
+  publicDir: './src/static'
 })
